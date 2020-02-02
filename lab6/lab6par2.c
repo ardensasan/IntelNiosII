b1275dfdfbe6449ac2e2e57d4f151ff79d44ba65
ardensasan <52578552+ardensasan@users.noreply.github.com>
2020-02-03 07:42:52 +0800

    Create lab6par2.c

---
+++ lab6/lab6par2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int LIST[8] = {7,6,3,3,14};
+*LEDs = 0xFF200000;
+
+int main()
+{
+    int n,index,lNum=0;
+    index = sizeof(LIST)/sizeof(LIST[0]);
+    for(n=0;n<=index;n++){
+        if(lNum < LIST[n]){
+            lNum = LIST[n];
+        }
+    }
+    printf("Largest Number: %d",lNum);
+    *LEDs = lNum;
+}
